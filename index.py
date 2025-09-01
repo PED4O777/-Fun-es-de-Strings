c2fd9666e6be2382db30c5ac8193505db99d97e7
PED4O777 <pedrobranpaiva@gmail.com>
2025-09-01 09:38:02 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,46 @@
+#ATIVIDADE 1
+
+# Loop = 0
+
+# while Loop <= 10:
+#     print(Loop)
+#     Loop +=1
+
+#ATIVIDADE 2
+# numero = 1
+
+# while numero <= 20:
+#     if numero % 2 != 0:
+#         numero += 1
+#         continue
+#     print(numero)
+#     numero += 1
+
+#ATIVIDADE 3
+# aluno = 1
+
+# while aluno <= 10:
+#     if aluno == 5:
+#         aluno += 1
+#         continue
+#     print("Chamando aluno número:", aluno)
+#     aluno += 1
+
+              
+#ATIVIDADE 4
+# tentativas = 0
+# senha_correta = "1234"
+
+# while True:
+#     senha = input("Digite a senha do caixa: ")
+#     tentativas += 1
+
+#     if senha == senha_correta:
+#         print("Acesso autorizado.")
+#         break
+
+#     elif tentativas == 3:
+#         print("Caixa bloqueado por excesso de tentativas.")
+#         break
+
+#     print("Senha incorreta. Tente novamente.")
